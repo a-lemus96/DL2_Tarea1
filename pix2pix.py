fc364ffb2249ea48e8a186808d9b928fecd64a12
a-lemus96 <alejandro.lemus@cimat.mx>
2022-09-13 19:22:18 -0500

    Lists of files have been validated

--- pix2pix.py
+++ pix2pix.py
@@ -20,16 +20,20 @@ R_LOSS_FACTOR = 10000
 EPOCHS = 100
 INITIAL_EPOCH = 0
 
-X_LEFT_FOLDER = "/home/est_posgrado_alejandro.lemus/DL2/Tarea1/training_data/left_X"
-X_RIGHT_FOLDER = "/home/est_posgrado_alejandro.lemus/DL2/Tarea1/training_data/right_X"
-Y_TARGET_FOLDER = "/home/est_posgrado_alejandro.lemus/DL2/Tarea1/training_data/target_Y"
+X_LEFT_FOLDER = "training_data/left_X/"
+X_RIGHT_FOLDER = "training_data/right_X/"
+Y_TARGET_FOLDER = "training_data/target_Y/"
 
 # Create lists of file directories
-left_x_files = np.array(glob(os.path.join(X_LEFT_FOLDER, '*.png')).sort())
-right_x_files = np.array(glob(os.path.join(X_RIGHT_FOLDER, '*.png')).sort())
-target_y_files = np.array(glob(os.path.join(Y_TARGET_FOLDER, '*.png')).sort())
+left_x_files = glob(os.path.join(X_LEFT_FOLDER, '*.png'))
+right_x_files = glob(os.path.join(X_RIGHT_FOLDER, '*.png'))
+target_y_files = glob(os.path.join(Y_TARGET_FOLDER, '*.png'))
 
-
+# Sort elements
+left_x_files.sort()
+right_x_files.sort()
+target_y_files.sort()
 
 for lx, rx, y in zip(left_x_files[:5], right_x_files[:5], target_y_files[:5]):
     print(f"{lx}\n{rx}\n{y}\n\n")
+    #print(ls)
